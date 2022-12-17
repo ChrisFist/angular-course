17cde17abcf109997601075aad34c6e164b16912
ChrisFist <35878355+ChrisFist@users.noreply.github.com>
2022-12-17 19:18:45 +0100

    do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,8 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  DoCheck,
-  Inject,
-  Injector,
-  OnInit,
-} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Course } from "./model/course";
 import { Observable } from "rxjs";
-import { AppConfig, CONFIG_TOKEN } from "./config";
-import { COURSES } from "../db-data";
+
 import { CoursesService } from "./courses/courses.service";
-import { createCustomElement } from "@angular/elements";
-import { CourseTitleComponent } from "./course-title/course-title.component";
 
 @Component({
   selector: "app-root",
@@ -26,21 +15,16 @@ export class AppComponent implements OnInit {
   private courseList: Course[];
 
   constructor(
-    private coursesService: CoursesService,
-    @Inject(CONFIG_TOKEN) private config: AppConfig,
-    private injector: Injector
-  ) {}
+    private coursesService: CoursesService // @Inject(CONFIG_TOKEN) private config: AppConfig,
+  ) //private injector: Injector
+  {}
 
   ngOnInit() {
-    const htmlElement = createCustomElement(CourseTitleComponent, {
-      injector: this.injector,
-    });
     this.courses$ = this.coursesService.loadCourses();
     this.courses$.subscribe((courses) => {
       this.coursesTotal = courses.length;
       this.courseList = courses;
     });
-    customElements.define("course-title", htmlElement);
   }
 
   onEditCourse() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { Injector, NgModule } from "@angular/core";
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
-import {CoursesModule} from './courses/courses.module';
-import {CourseTitleComponent} from './course-title/course-title.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { CoursesModule } from "./courses/courses.module";
+import { CourseTitleComponent } from "./course-title/course-title.component";
+import { AppConfig, CONFIG_TOKEN } from "./config";
+import { createCustomElement } from "@angular/elements";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        CourseTitleComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        CoursesModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, CourseTitleComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoursesModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  //  entryComponents: [CourseTitleComponent],
 })
 export class AppModule {
+  constructor(private injector: Injector) {
+    console.log("TEST");
 
+    const htmlElement = createCustomElement(CourseTitleComponent, {
+      injector: this.injector,
+    });
+    customElements.define("course-title", htmlElement);
+  }
 }
